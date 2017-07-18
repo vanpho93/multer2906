f55c50430eaaeefb4fbebe3b3a1aafda67a6e21b
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-18 21:21:56 +0700

    het

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const uploadConfig = require('./uploadConfig');
 
 const upload = uploadConfig.single('avatar');
+const arrayUpload = uploadConfig.array('avatar', 3);
+const fieldConfig = [{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 2 }];
+const fieldUpload = uploadConfig.fields(fieldConfig);
+
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -10,10 +14,25 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.render('home'));
 
 app.post('/upload', upload, (req, res) => {
+    // if (file) Nho handle
     const { originalname, mimetype } = req.file;
-    console.log(mimetype);
     const { username } = req.body;
     res.send(`Da nhan duoc file ${originalname} cua ${username}`);
+});
+
+app.post('/array', arrayUpload, (req, res) => {
+    const arrStr = req.files.map(e => e.originalname);
+    const { username } = req.body;
+    res.send(`Da nhan duoc file ${arrStr.join(', ')} cua ${username}`);
+});
+
+app.post('/field', fieldUpload, (req, res) => {
+    let arrStr = req.files.avatar.map(e => e.fieldname + ' ' + e.originalname);
+    arrStr = arrStr.concat(req.files.gallery.map(e => e.fieldname + ' ' + e.originalname));
+    const { username } = req.body;
+    res.send(`Da nhan duoc file ${arrStr.join(', ')} cua ${username}`);
+    // console.log(req.files);
+    // res.send('Test');
 });
 
 app.use((err, req, res, next) => {
