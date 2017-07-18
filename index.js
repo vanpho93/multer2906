dee38debc937b68df378577f4f4ecbb14a2bbd34
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-18 20:24:28 +0700

    error handle

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => cb(null, `${Date.now()}${file.originalname}`)
 });
 
-const upload = multer({ storage }).single('avatar');
+const limits = { fileSize: 50 * 1024 };
+const upload = multer({ storage, limits }).single('avatar');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -14,10 +15,18 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('home'));
+
 app.post('/upload', upload, (req, res) => {
     const { originalname } = req.file;
     const { username } = req.body;
     res.send(`Da nhan duoc file ${originalname} cua ${username}`);
+});
+
+app.use((err, req, res, next) => {
+    if(err.toString() === 'Error: File too large') {
+        return res.send('Chon file khac');
+    }
+    res.send(err.toString());
 });
 
 app.listen(3000, () => console.log('Server is running!'));
